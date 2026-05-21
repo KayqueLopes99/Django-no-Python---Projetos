0f228c1f8abb36d68615741af5869792de9e64ed
kayquelopes <kaiquelopes.601@gmail.com>
2026-05-21 09:44:15 -0300

    feat: add contact detail view and update index template; include header partial for navigation

--- Project_Agenda/contact/views/contact_views.py
+++ Project_Agenda/contact/views/contact_views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.http import Http404
+
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('id')[:10]
@@ -13,3 +15,22 @@ def index(request):
         'contact/index.html',
         context
     )
+    
+    
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    
+    contact_name = f"{single_contact.first_name} {single_contact.last_name} - "  
+    
+    context = {
+        'contact': single_contact,
+        'site_title': contact_name, 
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+)
